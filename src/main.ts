36e550b74c762e000dca46d2607dbc9d44df18f4
ammar <ammarraneez@gmail.com>
2021-11-27 17:41:49 +0530

    feat: create user

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    // remove off all other attribtues in body with aren't a part of the dto
+    whitelist: true,
+  }));
+
   await app.listen(3000);
 }
 bootstrap();
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './users.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    // create a User instance of typeorm repository
+    // DI doesn't fare well with generics, so @InjectRepository says
+    // that we need the User
+    @InjectRepository(User) private readonly repo: Repository<User>,
+  ) {}
+
+  create(email: string, password: string) {
+    // create a user entity instance, create first, then save, rather than direcly saving an object
+    // since any hook or validations are defined in the entity will not run
+    const user = this.repo.create({ email, password });
+
+    // save that instance to db
+    return this.repo.save(user);
+  }
+}
