0cde0517c286d87044a2478552565a49ab1e887d
ammar <ammarraneez@gmail.com>
2021-11-27 14:55:47 +0530

    feat: initial steps with typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    ReportsModule,
+
+    // typeorm with some configuration for entire project
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+
+      // db name
+      database: 'db.sqlite',
+
+      // the entities/models
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
