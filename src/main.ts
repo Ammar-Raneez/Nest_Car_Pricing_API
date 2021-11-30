8e828a27ea1e18a5069ef553d288c2d4bcc25cd2
ammar <ammarraneez@gmail.com>
2021-11-30 21:09:40 +0530

    feat: configure cookie session

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,22 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// use requires due to a compatibility issue with cookie session and tsconfig
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const cookieSession = require('cookie-session');
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    // remove off all other attribtues in body with aren't a part of the dto
-    whitelist: true,
-  }));
+
+  // the key will be used to encrypt the data in the cookie
+  app.useGlobalPipes(cookieSession({ keys: ['asdasdas'] }));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // remove off all other attribtues in body with aren't a part of the dto
+      whitelist: true,
+    }),
+  );
 
   await app.listen(3000);
 }
