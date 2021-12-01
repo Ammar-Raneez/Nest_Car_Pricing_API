30d2bd81ff19e6bf9c0379bc76ab846d0596e841
ammar <ammarraneez@gmail.com>
2021-12-01 11:11:50 +0530

    feat: globally scoped current user interceptor

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,11 +5,21 @@ import { User } from './users.entity';
 import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
 import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   // follow User entity schema
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService, AuthService, CurrentUserInterceptor],
+  providers: [
+    UsersService,
+    AuthService,
+
+    // globally scoped interceptor for this module
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CurrentUserInterceptor,
+    },
+  ],
 })
 export class UsersModule {}
