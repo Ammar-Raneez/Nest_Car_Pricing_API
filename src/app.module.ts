bafb1b3c069e00b32c900cb6cddd2f60937086ae
ammar <ammarraneez@gmail.com>
2021-12-01 16:24:27 +0530

    feat: handle different environments

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,30 +6,48 @@ import { ReportsModule } from './reports/reports.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/users.entity';
 import { Report } from './reports/reports.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
     ReportsModule,
+    ConfigModule.forRoot({
+      // use this config module for the entire app
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          entities: [User, Report],
+          synchronize: true,
+        };
+      },
+    }),
 
     // typeorm with some configuration for entire project
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
+    // TypeOrmModule.forRoot({
+    //   type: 'sqlite',
 
-      // db name
-      database: 'db.sqlite',
+    //   // db name
+    //   database: 'db.sqlite',
 
-      // the entities/models
-      entities: [User, Report],
+    //   // the entities/models
+    //   entities: [User, Report],
 
-      // the structure is created cuz of this attribute
-      // a SQL database is always kept as a rigid structure
-      // if we wanna change that structure (new/remove column etc) we must write a migration
-      // based on the entity typeorm would change the table structure automatically. This must therefore be
-      // used only in dev, when we aren't so sure about the structure, not in prod (ex: to prevent any accidental column being removed)
-      // Would use migrations in prod, if necessary
-      synchronize: true,
-    }),
+    //   // the structure is created cuz of this attribute
+    //   // a SQL database is always kept as a rigid structure
+    //   // if we wanna change that structure (new/remove column etc) we must write a migration
+    //   // based on the entity typeorm would change the table structure automatically. This must therefore be
+    //   // used only in dev, when we aren't so sure about the structure, not in prod (ex: to prevent any accidental column being removed)
+    //   // Would use migrations in prod, if necessary
+    //   synchronize: true,
+    // }),
   ],
   controllers: [AppController],
   providers: [AppService],
