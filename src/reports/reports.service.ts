7598fc56a4129cb1a0fa27a2344a69501f02d8a6
ammar <ammarraneez@gmail.com>
2021-12-02 20:35:12 +0530

    feat: get estimate

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/users/users.entity';
 import { Repository } from 'typeorm';
 import { CreateReportDto } from './dtos/create-report.dto';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 import { Report } from './reports.entity';
 
 @Injectable()
@@ -27,5 +28,20 @@ export class ReportsService {
 
     report.approved = approved;
     return this.repo.save(report);
+  }
+
+  createEstimate({ make, model, lng, lat, year, mileage }: GetEstimateDto) {
+    return this.repo.createQueryBuilder()
+      .select('AVG(price)', 'price')
+      .where('make == :make', { make }) // :make will take the value we specify in the object
+      .andWhere('model == :model', { model })
+      .andWhere('lng -:lng BETWEEN -5 AND 5', { lng })
+      .andWhere('lat -:lat BETWEEN -5 AND 5', { lat })
+      .andWhere('year -:year BETWEEN -3 AND 3', { year })
+      .andWhere('approved IS TRUE')
+      .orderBy('ABS(mileage - :mileage)', 'DESC')
+      .setParameters({ mileage }) // orderBy doesn't have a second arg to take the param
+      .limit(3) // take only the top 3
+      .getRawOne();
   }
 }
